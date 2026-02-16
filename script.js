42ea46cd9b0a3e6434c673c6be728d0fbbe3f187
UmmuHusnul <ummuhusnul124@gmail.com>
2026-02-17 03:38:21 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+    const question = item.querySelector(".faq-question");
+
+    question.addEventListener("click", () => {
+
+        // Close other open items (optional professional behavior)
+        faqItems.forEach(otherItem => {
+            if (otherItem !== item) {
+                otherItem.classList.remove("active");
+            }
+        });
+
+        // Toggle current item
+        item.classList.toggle("active");
+    });
+});
